b0784baec8e61c11513643ee83990a8c5905c735
Christian Babsek <christian.babsek@syna.de>
2024-03-20 15:50:09 +0100

    V2

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,16 @@ dependencies {
 }
 
 kotlin {
+    sourceSets {
+        commonMain {
+            dependencies {
+                implementation("io.arrow-kt:arrow-core:1.2.1")
+            }
+        }
+    }
+
     js {
         binaries.executable()
+        generateTypeScriptDefinitions()
     }
 }
--- src/commonMain/kotlin/Main.kt
+++ src/commonMain/kotlin/Main.kt
@@ -1,10 +1,17 @@
 package de.syna.hoechstdigital.demo
 
+import arrow.core.Either
+import arrow.core.left
+
 @OptIn(kotlin.js.ExperimentalJsExport::class)
 @JsExport
 fun main() {
     val rule1 = Rule("Regel 1")
     val rule2 = Rule("Regel 2")
+
+    val either1 = "Wert".left()
+
+    console.log(either1)
 
     kotlin.js.console.log(processRules(arrayOf(rule1, rule2)))
 }
